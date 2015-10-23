dc2926c819fafe766bed30e0e6316fb8b7d52e24
inkblotty <inkblot.and.blush@gmail.com>
2015-10-22 22:42:59 -0600

    added default task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -62,3 +62,21 @@ gulp.task('styles', function() {
 		.pipe(minifyCSS())
 		.pipe(gulp.dest('./build/styles/'));
 });
+
+// default task
+gulp.task('default', ['imagemin', 'htmlpage', 'scripts', 'styles'], function() {
+	// watch for HTML changes
+	gulp.watch('./src/*.html', function() {
+		gulp.run('htmlpage');
+	});
+
+	// watch for JS changes
+	gulp.watch('./src/scripts/*.js', function() {
+		gulp.run('jshint', 'scripts');
+	});
+
+	// watch for CSS changes
+	gulp.watch('./src/styles/*.scss', function() {
+		gulp.run('styles');
+	});
+});
