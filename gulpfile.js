f786a5f8ee2478042bf0919ab2c00cd2f2a29a5c
inkblotty <inkblot.and.blush@gmail.com>
2015-10-22 21:38:13 -0600

    added task 'jshint' to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,8 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
 // JS hint task
+gulp.task('jshint', function() {
+	gulp.src('./src/scripts/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
