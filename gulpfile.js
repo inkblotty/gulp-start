1e0782daa9bf7eb4c39b80a00afafaf4c59d9329
inkblotty <inkblot.and.blush@gmail.com>
2015-10-22 21:34:36 -0600

    created gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+// gulpfile
+
+// include gulp
+var gulp = require('gulp');
+
+// include plug-ins
+var jshint = require('gulp-jshint');
+
+// JS hint task
--- readme.txt
+++ readme.txt
@@ -1,3 +1,4 @@
 Start for any gulp projects.
 
 --Remove any unneeded file types from .gitignore
+--Add gulpfile.js to .gitignore
